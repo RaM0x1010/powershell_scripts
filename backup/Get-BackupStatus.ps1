a983405d3d96e5c29236ec661df0dc06d9400364
ram0x1010 <ramirzaliev@gmail.com>
2023-01-13 15:59:07 +0300

    added new file

---
+++ backup/Get-BackupStatus.ps1
@@ -0,0 +1,24 @@
+<#
+
+ .SYNOPSIS
+  Checks all type of backups in infrastructure by given incoming data.
+ 
+ .SYNTAX
+ 
+ 
+ .DESCRIPTION
+  Do the checking backups job. Making some report about status of all backups and sends to email to the sysas.
+
+ .Example
+   # Show a default display of this month.
+   Show-Calendar
+
+ .Example
+   # Display a date range.
+   Show-Calendar -Start "March, 2010" -End "May, 2010"
+
+ .Example
+   # Highlight a range of days.
+   Show-Calendar -HighlightDay (1..10 + 22) -HighlightDate "December 25, 2008"
+#>
+
