d936c0cbe5b09f4b49271e0a7871bebe0cef3d9a
ram0x1010 <ramirzaliev@gmail.com>
2023-01-26 12:41:42 +0300

    veeam module added

--- backup/Get-BackupStatus.ps1
+++ backup/Get-BackupStatus.ps1
@@ -21,4 +21,73 @@
    # Highlight a range of days.
    Show-Calendar -HighlightDay (1..10 + 22) -HighlightDate "December 25, 2008"
 #>
-Import-Module -Name ./modules/PSGetInfoVeeamBackupJobs/PSGetInfoVeeamBackupJobs.psm1
+
+#Import-Module -Name .\modules\PSGetInfoVeeamBackupJobs\PSGetInfoVeeamBackupJobs.psm1
+##Import-Module -Name .\modules\PSGetInfoMSSQLTasks\PSGetInfoMSSQLTasks.psm1
+#Import-Module -Name .\modules\PSGetInfoPostgreSQLTasks\PSGetInfoPostgreSQLTasks.psm1
+
+function Get-BackupsLastStatuses {
+  [CmdletBinding()]
+
+  Param(
+      [string[]]$Server = "localhost"
+  )
+  
+
+  $result = @()
+  
+  try {
+      $script_path = ".\backup\Get-BackupsLastStatuses.ps1"
+      $result = Invoke-Command -ComputerName $Server -FilePath $script_path
+      return $result
+  }
+  catch {
+      return $("Error")
+  }  
+}
+
+
+$servers = @("spb-buh-bkp-3","r-bkp")
+                
+    $result += [PSCustomObject]@{
+                    shop = $op.name;
+                  server = "-";
+                    name = "-";
+                    path = "-";
+                  status = "-";
+                message = "-";
+            }
+
+    # foreach($srv in $servers){
+    #     $statuses = checkVeeamBackups -Server $srv
+    #     $statuses.Length
+    # }
+
+    foreach($srv in $servers){
+        $statuses = checkVeeamBackups -Server $srv                    
+        if($statuses.Length){                        
+        for ($i = 0; $i -lt $statuses[1].Count; $i++) {
+            
+            $result += [PSCustomObject]@{
+                        shop = $srv;
+                      server = $statuses[1][$i].OrigJobName;
+                        name = $statuses[1][$i].JobType;                                   
+                        path = $("DR Site");#"$($statuses[1][$i].GetJob().TargetDir.ToString())";
+                      status = $statuses[1][$i].Result;
+                    message = $statuses[1][$i].EndTimeUTC;
+                }
+        }
+        for ($i = 0; $i -lt $statuses[0].Count; $i++){
+            if($statuses[0][$i].Name -eq "r-fs-1"){
+                $result += [PSCustomObject]@{
+                        shop = $srv;
+                      server = $statuses[0][$i].Name;
+                        name = $statuses[0][$i].JobType;
+                        path = [string]$statuses[0][$i].TargetDir;
+                      status = $("ok");#$statuses[0][$i].GetLastResult();
+                    message = $("none");#$statuses[0][$i].GetLastBackup().LastPointCreationTime.ToString(); #"test";
+              }
+            }
+        }
+    }else{$("Current length is " + $statuses.Length)}
+}
--- modules/PSGetInfoVeeamBackupJobs/PSGetInfoVeeamBackupJobs.psm1
+++ modules/PSGetInfoVeeamBackupJobs/PSGetInfoVeeamBackupJobs.psm1
@@ -1,1 +1,64 @@
-﻿
+﻿##
+##
+##
+
+
+function Get-BackupsLastStatuses {
+    [CmdletBinding()]
+
+    Param(
+        [string]$Server = "localhost"
+    )
+
+    try{
+        Invoke-Command -ComputerName $Server -ScriptBlock{
+
+            Add-PSSnapin -Name VeeamPSSnapin
+
+            if(!(Get-VBRServerSession)){
+                Connect-VBRServer #$Server
+            }
+
+            $all_jobs = Get-VBRJob | Where-Object {($_.IsScheduleEnabled -eq $true) -and ($_.ScheduleOptions.NextRun -ne "")}
+            $all_sessions = Get-VBRBackupSession
+            $last_status_backup_copy_job = @()
+            $last_status_vms_in_backup_jobs = @()
+
+
+            #Get all statuses of backup jobs and backup copy jobs
+
+            foreach($backup_job in $all_jobs){
+                Switch ($backup_job.JobType){
+                    "Backup"{
+                        #$last_status_vms_in_backup_jobs += Get-AllObjectInTaskSession -Session $($all_sessions | Where-Object {$backup_job.Id -eq $_.JobId} | Sort-Object -Property EndTimeUTC -Descending | Select-Object -First 1).OriginalSessionId
+                        $task_session_info = Get-VBRTaskSession -Session $($all_sessions | Where-Object {$backup_job.Id -eq $_.JobId} | Sort-Object -Property EndTimeUTC -Descending | Select-Object -First 1).OriginalSessionId
+                        #$last_status_vms_in_backup_jobs += 
+                        foreach($task_session in $task_session_info){
+                            $last_status_vms_in_backup_jobs += [PSCustomObject]@{
+                                JobName = $task_session.JobName;
+                                VMName = $task_session.Name;
+                                BackupEndTimeLocal = $task_session.Info.Progress.StopTimeLocal;
+                                LastStatus = $task_session.Status;
+                            }
+                        }
+
+                    }
+                    "BackupSync"{
+                        if($backup_job.GetLastResult() -ne "None"){
+                            $last_status_backup_copy_job = $last_status_backup_copy_job + ($all_sessions | Where-Object {$backup_job.Id -eq $_.JobId} | Sort-Object -Property EndTimeUTC -Descending)[0]
+                        }else{
+                            $last_status_backup_copy_job = $last_status_backup_copy_job + ($all_sessions | Where-Object {$backup_job.Id -eq $_.JobId} | Sort-Object -Property EndTimeUTC -Descending)[1]
+                        }
+                    }
+                }    
+            }
+
+            return @($last_status_vms_in_backup_jobs, $last_status_backup_copy_job)
+        }
+    }
+    catch{
+        return $("Error")
+    } 
+
+}
+
