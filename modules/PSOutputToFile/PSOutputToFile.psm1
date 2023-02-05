282cc6bbfbf4516b5e2129e8c71228311f434718
ram0x1010 <ramirzaliev@gmail.com>
2023-02-05 21:01:44 +0300

    changes

--- modules/PSOutputToFile/PSOutputToFile.psm1
+++ modules/PSOutputToFile/PSOutputToFile.psm1
@@ -1,7 +1,29 @@
-$manifest = @{
-    Path              = '.\modules\PSOutputToFile\PSOutputToFile.psd1'
-    RootModule        = 'PSOutputToFile.psm1' 
-    Author            = 'Mirzaliev Ruslan'
+
+
+
+function Out-HTMLCheckList {
+    [CmdletBinding()]
+
+    param (
+        [string]$eeee
+    )
+    
 }
 
-New-ModuleManifest @manifest
+
+function Out-ExcelCheckList {
+    [CmdletBinding()]
+
+    param (
+        [string]$excel
+    )
+    
+}
+
+# $manifest = @{
+#     Path              = '.\PSOutputToFile\PSOutputToFile.psd1'
+#     RootModule        = 'PSOutputToFile.psm1'
+#     Author            = 'Mirzaliev Ruslan'
+# }
+
+# new-ModuleManifest @manifest
